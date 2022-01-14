038a2f16cd1684083e48db38faff790be7148128
omarkandil <omarkandildw@gmail.com>
2022-01-14 15:01:45 +0100

    last update

--- js.js
+++ js.js
@@ -7,36 +7,66 @@ var radioCheck=document.getElementById("radiobtn");
 var input = document.getElementsByTagName("input");
 var langue=document.getElementById("langue");
 var table = document.getElementById("table");
+var tbody = document.getElementsByTagName("tbody");
 var type=document.getElementsByClassName("type");
 var email = document.getElementById("email");
+var div = document.getElementById("btn3");
 
+var list = [];
 
+// ++++++++++ Class +++++++++++
+class Ouvrage 
+{
+    constructor(title, auteur, prix, date, langue, type,email){
 
-
-
-// console.log(type.value);
-
-       
-
-        let list = [];
-
-function validation(e){
-    var no_valide=0;
-    //////require:
-
-
+        this.title=title;
+        this.auteur=auteur;
+        this.prix=prix;
+        this.date=date;
+        this.langue=langue;
+        this.type=type;
+        this.email=email;
+        
+    }
     
+    detailOuvrage() {
+        return (`L'ouvrage ${this.title} est un ${this.type}  en langue ${this.langue} écrit par  ${this.auteur}  et publié le ${this.date} Le prix de ${this.titre} , est de  ${this.prix} DHs`)
+    }
+    
+}
+
+// +++++++++++++ Validation +++++++++++
+var list2 = "";
+list2 = JSON.parse(localStorage.getItem("list"));
+
+
+
+
+if(list2!=null){
+    list = list2;
+}
+
+function validation(){
+    type = document.querySelector("input[name=season]:checked");
+    var  myouvrage = new Ouvrage (title.value, auteur.value ,prix.value,date.value,langue.value,type.value,email.value,type);
+        
+        
+    var no_valide=0;
+
+    // ++++ Required ++++
+
     for(var i = 0; i<4;i++){
         if (input[i].value == ""){
             input[i].style.borderColor="red";
             no_valide++;
         }
         else{
-            // input[i].nextElementSibling.innerHTML="valider";
+
             input[i].style.borderColor="green"
         }
     }     
-    //////require RADIO
+
+    // ++++++++ require RADIO +++++++++
     var getSelectedValue = document.querySelector(   
          'input[name="season"]:checked');   
         
@@ -49,8 +79,7 @@ function validation(e){
         no_valide++; 
     } 
     
-    // email//////////
-  
+    // +++++++ Email +++++++++
 
     var emailRegex = new RegExp(/^[a-z\d\.]+@[a-z\d]+\.([a-z]{2,8})(\.[a-z]{2,8})?$/);
     let checkEmail = email.value;
@@ -60,17 +89,33 @@ function validation(e){
           console.log("Email is valid")
     } else {
         alert("Email is not valid")
+        no_valide++;
         
   }
 
-    //////require drop
+
+//    +++++++date+++++++++++++
+
+
+       if(date.value=="")
+       {
+           alert("the date is not Valid")
+           no_valide++;
+             }
+           
+           else {
+               console.log("the date is valid")
+           }
+     
+
+    // +++++require drop +++++++
 
     if(langue.value=="0")
     {
         lng.innerHTML="Please select an option!";
         langue.style.borderColor="red"
         no_valide++;
-        console.log("langue choisi")
+        
     }
     else{
         lng.innerHTML="valider";
@@ -78,12 +123,12 @@ function validation(e){
         
     }
 
-    /////////////////prix
+    // ++++++ prix ++++++
 
 
-    var prix = new RegExp(/^\d+(,\d{1,2})?$/)
-    var checkPrice = document.getElementById("prix").value;
-    var valid = prix.test(checkPrice)
+    var prixRegExp = new RegExp(/^\d+(,\d{1,2})?$/)
+    var price = document.getElementById("prix").value;
+    var valid = prixRegExp.test(price)
 
     if (valid==true)
     {
@@ -92,27 +137,38 @@ function validation(e){
 
     else {
         alert("The Price is not Valid")
+        no_valide++;
        
     }
 
 
-
-
-
-    // // // // // // // // // class
-
-
-
-
-    
     ///////////////table/////////////////
 
 
+        // ++++++Insert row+++++++
+        if(no_valide==0)
+        {
 
-if(no_valide==0)
-{
+    
 
-    var newRow = table.insertRow(-1);
+        // i cannot tell
+        // for(i=0;i<type.length;i++)
+        // {
+        //     if(type[i].checked)
+        //     {
+        //         CellType=type[i].value;
+        //     }
+        // } 
+
+       
+        list.push(myouvrage);
+        tri() 
+        
+
+        localStorage.setItem("list", JSON.stringify(list));
+        tbody.innerHTML="";
+        for(var i=0; i<list.length; i++){
+            var newRow = table.insertRow(-1);
             cell1 = newRow.insertCell(0);
             cell2 = newRow.insertCell(1);
             cell3 = newRow.insertCell(2);
@@ -121,54 +177,62 @@ if(no_valide==0)
             cell6 = newRow.insertCell(5);
             cell7 = newRow.insertCell(6);
             cell8 = newRow.insertCell(7);
-        var CellType="";
-        for(i=0;i<type.length;i++)
-        {
-            if(type[i].checked)
-            {
-                CellType=type[i].value;
-            }
+            var CellType=document.querySelector("input[name=season]:checked").value;
+            cell1.innerHTML = list[i].title;
+            cell2.innerHTML = list[i].auteur;
+            cell3.innerHTML = list[i].prix;
+            cell4.innerHTML = list[i].date;
+            cell5.innerHTML = list[i].langue;
+            cell6.innerHTML = list[i].email;
+            cell7.innerHTML = list[i].type;
+            cell8.innerHTML ='<input type="button"  value="Edit" onclick="EditRow(this)" class="btn1">'
+            + '<input type="button" onclick="deleteRow(this)"  value="Delete" class="btn2">';
         }
+        
+        
+        resetUnputs()
+}
 
-        cell1.innerHTML = title.value;
-        cell2.innerHTML = auteur.value;
-        cell3.innerHTML = prix.value;
-        cell4.innerHTML = date.value;
-        cell5.innerHTML = langue.options[langue.selectedIndex].value;
-        cell6.innerHTML = email.value;
-        cell7.innerHTML = CellType;
-        cell8.innerHTML ='<input type="button"  value="Edit" onclick="EditRow(this)" class="btn1">'
-        + '<input type="button" onclick="deleteRow(this)"  value="Delete" class="btn2">';
+
+function tri(){
+    list.sort(function(a,b){
+        if(a.title<b.title){
+            return -1;
+        }
+  
+    })
+}
+
+for(let i = 0; i < list.length; i++){
+// console.log(list[i].detailOuvrage());s
+} 
+
+
+    console.log("this is the list log: "+ list);
+    console.log("this is the myOverage log: "+myouvrage);
 
 }
 
 
-type = document.querySelector("input[name=season]:checked");
-
-let  myOuvrage = new Ouvrage (title.value, auteur.value ,prix.value,date.value,langue.value, type.value, email.value);
-
-
-list.push(myOuvrage);
-console.log(list);
-console.log(myOuvrage);
-
-}
-
-document.getElementsByTagName("form")[0].addEventListener('submit', validation);
+// document.getElementsByTagName("form")[0].addEventListener('submit', validation);
  
 function EditRow(r){
+
    ed = r.parentNode.parentNode
     
    if(r.value=="Edit"){
-       
+
   document.getElementById("title").value = ed.cells[0].innerHTML;
   document.getElementById("auteur").value= ed.cells[1].innerHTML;
-  document.getElementById("prix").value= ed.cells[2].innerHTML;
+  document.getElementById("prix").value= ed.cells[2].innerHTML; 
   document.getElementById("date").value= ed.cells[3].innerHTML;
   document.getElementById("langue").value=ed.cells[4].innerHTML;
   document.getElementById("email").value=ed.cells[5].innerHTML;
   document.getElementsByClassName("type").value=ed.cells[6].innerHTML;
-    
+     resetUnputs()
+
+
+
     r.value = "save"
     document.getElementById("btn").setAttribute("disabled" , "true");
    }
@@ -179,18 +243,16 @@ function EditRow(r){
     ed.cells[3].innerHTML= document.getElementById("date").value
     ed.cells[4].innerHTML= document.getElementById("langue").value
     ed.cells[5].innerHTML= document.getElementById("email").value
-    CellType="";
-    for(i=0;i<type.length;i++)
-    {
-        if(type[i].checked)
-        {
-            CellType=type[i].value;
-        }
-    }
+    ed.cells[6].innerHTML= document.querySelector("input[name=season]:checked").value
 
-    
-    ed.cells[6].innerHTML= CellType
-    alert(CellType)
+    // for(i=0;i<type.length;i++)
+    // {
+    //     if(type[i].checked)
+    //     {
+    //         CellType=type[i].value;
+    //     }
+    // }
+
 
     r.value = "Edit"
 
@@ -200,6 +262,7 @@ function EditRow(r){
     }  
     
    }
+  
 
     }
 
@@ -215,33 +278,37 @@ function deleteRow(r) {
   
 }
 
-// // // // // // // // class///////////////////
 
-class Ouvrage {
-    constructor(title, auteur,prix,date,langue,type,email){
 
-        this.title=title;
-        this.auteur=auteur;
-        this.prix=prix;
-        this.date=date;
-        this.langue=langue;
-        this.type=type;
-        this.email=email;
-
-    }
-    
-    DetailOuvrage() {
-        return (`L'ouvrage ${this.title} est un ${this.type}  en langue ${this.langue} écrit par  ${this.auteur}  et publié le ${this.date} Le prix de ${this.titre} , est de  ${this.prix} DHs`)
-    }
-}
-
-const ouvrage1 = new Ouvrage()
-alert(ouvrage1.DetailOuvrage())
+// const ouvrage1 = new Ouvrage()
+// alert(ouvrage1.DetailOuvrage())
        
 
 
+// Reset Function
+function resetUnputs()
+{
+    document.getElementById("title").value = ""
+    document.getElementById("auteur").value = ""
+    document.getElementById("prix").value = ""
+    document.getElementById("date").value = ""
+    document.getElementById("langue").value = ""
+    document.getElementById("email").value = ""
+    document.querySelector("input[value=Roman]").checked = false
+    document.querySelector("input[value=Essai]").checked = false
+    document.querySelector("input[value=Bande-Dessinée]").checked = false
+}
 
-let btn = document.getElementById('btn3')
-btn.addEventListener('click', function() {
-    window.print(), id='="noPrint"'
-})
+btn3.addEventListener("click", function printData() {
+    var div = document.getElementById("table");
+    newwin = window.open("");
+    newwin.document.write(div.outerHTML);
+    newwin.print();
+    newwin.close();
+  });
+
+
+// var btn = document.getElementById('btn3')
+// btn.addEventListener('click', function() {
+//     window.print(), id='="noPrint"'
+// })
